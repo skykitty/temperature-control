8817af943b9eae7fe068be578a17a3504c7265af
TheSkyKitty <jonada.ssai@gmail.com>
2023-08-08 22:00:05 +0200

    DANIKU, UKAZ SE :p

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [temperature, setTemperature] = useState(10);
+  const [tempColor, setTempColor] = useState("cold");
+
+  const increaseTemperature = (e) => {
+    if (temperature === 30) return;
+    console.log("super");
+    let interval = setInterval(() => {
+      console.log("super1");
+      const newTemperature = temperature + 1;
+      setTempColor(() => {
+        if (temperature + 1 >= 15) {
+          return "hot";
+        }
+      });
+
+      setTemperature((prev) => prev + 1);
+    }, 50);
+    e.target.addEventListener("mouseup", () => {
+      console.log("super2");
+      clearInterval(interval);
+    });
+  };
+
+  const decreaseTemperature = () => {
+    if (temperature === 0) return;
+
+    const newTemperature = temperature - 1;
+    if (newTemperature < 15) {
+      setTempColor("cold");
+    }
+    setTemperature(newTemperature);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className={`temperature-display ${tempColor}`}>
+          {temperature}°C
+        </div>
+      </div>
+      <div className="button-container">
+        <button onMouseDown={increaseTemperature}>+</button>
+        <button onClick={() => decreaseTemperature()}>-</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
